d715f2abf22697e7d9c09dcd3187900b44980453
ANSANJAY <anamika.sanjay16@gmail.com>
2023-10-02 20:53:04 -0500

    log level

--- 1_loglevel/hello.c
+++ 1_loglevel/hello.c
@@ -4,13 +4,13 @@
 MODULE_LICENSE("GPL");
 static int test_hello_init(void)
 {
-    printk("\001""4""%s: In init\n", __func__);
+    printk("\001""4""%s: In init\n", __func__); //warning
     return 0;
 }
 
 static void test_hello_exit(void)
 {
-    printk("\001""2""%s: In exit\n", __func__);
+    printk("\001""2""%s: In exit\n", __func__); //crit
 }
 
 module_init(test_hello_init);
